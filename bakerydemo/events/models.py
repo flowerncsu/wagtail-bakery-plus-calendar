9f440386ebed712d13850b0130e14f45ff2dcb8c
Dmitriy Chukhin <dchukhin@gmail.com>
2017-07-13 17:33:29 -0400

    add EventDetailPage model

--- bakerydemo/events/models.py
+++ bakerydemo/events/models.py
@@ -1,5 +1,14 @@
-from wagtail.wagtailcore.models import Page
 from django.db import models
+
+from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import StreamField
+from wagtail.wagtailadmin.edit_handlers import (
+    FieldPanel, StreamFieldPanel
+)
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtailsearch import index
+
+from bakerydemo.base.blocks import BaseStreamBlock
 
 
 class EventIndexPage(Page):
@@ -14,3 +23,34 @@ class EventIndexPage(Page):
         related_name='+',
         help_text='Landscape mode only; horizontal width between 1000px and 3000px.'
     )
+
+
+class EventDetailPage(Page):
+    introduction = models.TextField(
+        help_text='Text to describe the event',
+        blank=True
+    )
+    body = StreamField(
+        BaseStreamBlock(), verbose_name="Event body", blank=True
+    )
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text='Landscape mode only; horizontal width between 1000px and 3000px.'
+    )
+    datetime = models.DateTimeField()
+
+    content_panels = Page.content_panels + [
+        FieldPanel('datetime', classname="full"),
+        FieldPanel('introduction', classname="full"),
+        ImageChooserPanel('image'),
+        StreamFieldPanel('body'),
+    ]
+    search_fields = Page.search_fields + [
+        index.SearchField('title'),
+        index.SearchField('body'),
+    ]
+    parent_page_types = ['EventIndexPage']
